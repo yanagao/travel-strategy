2b5b24997a967dbd2d6986f7d29051074083fbef
ya gao <gaoya@inftech.ai>
2023-12-04 11:27:19 +0800

    update

--- src/components/SerchForm/index.tsx
+++ src/components/SerchForm/index.tsx
@@ -1,82 +1,37 @@
-import React, { useState } from 'react';
-import { fetchData } from '@/apis/openai';
+import { useState } from 'react';
 import { DatePicker, Input } from 'antd';
 import { Moment } from 'moment';
-import { marked } from 'marked';
-import * as FileSaver from 'file-saver';
-import * as XLSX from 'xlsx';
 import './index.less';
 
 const { RangePicker } = DatePicker;
 
-const SearchForm = () => {
+interface SearchFormProps {
+  handleSubmit: (
+    dateRange: [Moment | null, Moment | null],
+    destination: any,
+    origin: any,
+    backtracking: any
+  ) => void;
+}
+
+const SearchForm = ({ handleSubmit }: SearchFormProps) => {
   const [destination, setDestination] = useState('');
   const [origin, setOrigin] = useState('');
   const [backtracking, setBacktracking] = useState('');
   const [dateRange, setDateRange] = useState<[Moment | null, Moment | null]>([null, null]);
-  const [resultText, setResultText] = useState();
 
   const handleDateChange = (dates: [Moment | null, Moment | null]) => {
     setDateRange(dates);
   };
 
-  const handleSubmit = async (event: React.FormEvent) => {
+  const onSubmit = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
-    // 获取日期范围的字符串表示
-    const startDate = dateRange[0] ? dateRange[0].format('YYYY-MM-DD') : '';
-    const endDate = dateRange[1] ? dateRange[1].format('YYYY-MM-DD') : '';
-    // 确保日期已选择
-    if (!startDate || !endDate) {
-      alert('请选择开始和结束日期！');
-      return;
-    }
-    console.log({ destination, origin, startDate, endDate });
-    const prompt = `你是我的旅游助力和向导；我的计划是在${startDate}到${endDate}从${origin}出发去${destination}旅游最后返回${backtracking}；你帮我制定一个完整的旅游攻略，包括必游景点、必吃美食、预计费用，以及每日行程规划；请用markdown表格的方式输出。`
-    const res = await fetchData(prompt);
-    setResultText(res.choices[0].text);
-    console.log(res.choices[0].text);
+    handleSubmit(dateRange, destination, origin, backtracking);
   };
-
-  const convertMarkdownToWorksheet = (resultText: string): XLSX.WorkSheet => {
-    // 使用Markdown解析器转换表格
-    const htmlString = marked.parse(resultText);
-    const el = document.createElement('html');
-    el.innerHTML = htmlString;
-
-    // 提取HTML表格到二维数组
-    const data: string[][] = [];
-    const rows = el.querySelectorAll('table tr');
-    rows.forEach(tr => {
-      const rowData: string[] = [];
-      tr.querySelectorAll('td, th').forEach(td => rowData.push(td.innerText));
-      data.push(rowData);
-    });
-
-    // 使用SheetJS工具将二维数组转换为工作表
-    return XLSX.utils.aoa_to_sheet(data);
-  };
-
-  const handleExport = () => {
-    // 转换Markdown表格为Worksheet
-    const ws: XLSX.WorkSheet = convertMarkdownToWorksheet(resultText);
-
-    // 创建工作簿并添加工作表
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-    // 生成Excel文件
-    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
-    const data: Blob = new Blob([excelBuffer], {
-      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8',
-    });
-
-    // 保存文件
-    FileSaver.saveAs(data, `${destination}strategy.xlsx`);
-  }
 
   return (
     <div className="form-container">
-      <form onSubmit={handleSubmit} className='form'>
+      <form className='form'>
         <div className='input-group'>
           <label htmlFor="destination" className='label'>目的地:</label>
           <Input
@@ -118,12 +73,8 @@ const SearchForm = () => {
           />
         </div>
 
-        <button type="submit" className='button'>搜索</button>
+        <button className='button' onClick={onSubmit}>搜索</button>
       </form>
-      {resultText && <div id="resultText" className='resultContainer'>
-        <div dangerouslySetInnerHTML={{ __html: marked.parse(resultText) }} />
-        <button className='button' onClick={handleExport}>导出为Excel</button>
-      </div>}
     </div>
   );
 };
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,85 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 // import { fetchData } from '@/apis/openai';
 import SearchForm from '@/components/SerchForm';
 import './index.less';
+import { fetchData } from '@/apis/openai';
+import { marked } from 'marked';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+import { Moment } from 'moment';
 
 function Index() {
+  const [resultText, setResultText] = useState();
 
   useEffect(() => {
     // fetchData();
   }, []);
+
+  const handleSubmit = async (
+    dateRange: [Moment | null, Moment | null],
+    destination: any,
+    origin: any,
+    backtracking: any
+  ) => {
+    // 获取日期范围的字符串表示
+    const startDate = dateRange[0] ? dateRange[0].format('YYYY-MM-DD') : '';
+    const endDate = dateRange[1] ? dateRange[1].format('YYYY-MM-DD') : '';
+    // 确保日期已选择
+    if (!startDate || !endDate) {
+      alert('请选择开始和结束日期！');
+      return;
+    }
+    console.log({ destination, origin, startDate, endDate });
+    const prompt = `你是我的旅游助力和向导；我的计划是在${startDate}到${endDate}从${origin}出发去${destination}旅游最后返回${backtracking}；你帮我制定一个完整的旅游攻略，包括必游景点、必吃美食、预计费用，以及每日行程规划（最好是计划到具体小时）；请用markdown表格的方式输出。`
+    const res = await fetchData(prompt);
+    setResultText(res.choices[0].text);
+    console.log(res.choices[0].text);
+  };
+
+  const convertMarkdownToWorksheet = (resultText: string): XLSX.WorkSheet => {
+    // 使用Markdown解析器转换表格
+    const htmlString = marked.parse(resultText);
+    const el = document.createElement('html');
+    el.innerHTML = htmlString;
+
+    // 提取HTML表格到二维数组
+    const data: string[][] = [];
+    const rows = el.querySelectorAll('table tr');
+    rows.forEach(tr => {
+      const rowData: string[] = [];
+      tr.querySelectorAll('td, th').forEach(td => rowData.push(td.innerText));
+      data.push(rowData);
+    });
+
+    // 使用SheetJS工具将二维数组转换为工作表
+    return XLSX.utils.aoa_to_sheet(data);
+  };
+
+  const handleExport = () => {
+    // 转换Markdown表格为Worksheet
+    const ws: XLSX.WorkSheet = convertMarkdownToWorksheet(resultText);
+
+    // 创建工作簿并添加工作表
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    // 生成Excel文件
+    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const data: Blob = new Blob([excelBuffer], {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8',
+    });
+
+    // 保存文件
+    FileSaver.saveAs(data, 'strategy.xlsx');
+  }
+
   return (
     <div className="index-page">
-      <SearchForm />
+      <SearchForm handleSubmit={handleSubmit} />
+      {resultText && <div id="resultText" className='result-container'>
+        <div dangerouslySetInnerHTML={{ __html: marked.parse(resultText) }} />
+        <button className='button' onClick={handleExport}>导出为Excel</button>
+      </div>}
     </div>
   );
 }
